1b33395ca92d1ae2bfe5b44a6e84a8d7a1933584
youssefalb <yousalb891@student.polsl.pl>
2023-05-22 10:59:55 +0200

    Trying multiple hyperparameters and plotting

--- custom_collate.py
+++
@@ -1,5 +0,0 @@
-from torch.utils.data.dataloader import default_collate
-
-def custom_collate(batch):
-    batch = [sample for sample in batch if sample is not None]
-    return default_collate(batch)
