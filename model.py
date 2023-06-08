c90d10c239f770d14c9365de154e1d3f7bcdb645
youssefalb <yousalb891@student.polsl.pl>
2023-06-08 18:02:58 +0200

    some progress

--- model.py
+++ model.py
@@ -1,20 +1,18 @@
 import torch
 import torch.nn as nn
 
-class Recommender(torch.nn.Module):
+class Recommender(nn.Module):
     def __init__(self, num_users, num_isbns, embedding_dim):
         super(Recommender, self).__init__()
-        self.user_embedding = torch.nn.Embedding(num_embeddings=num_users, embedding_dim=embedding_dim)
-        self.isbn_embedding = torch.nn.Embedding(num_embeddings=num_isbns, embedding_dim=embedding_dim)
-        self.fc1 = torch.nn.Linear(embedding_dim * 2, 64)
-        self.fc2 = torch.nn.Linear(64, 32)
-        self.fc3 = torch.nn.Linear(32, 1)
+        self.user_embedding = nn.Embedding(num_embeddings=num_users, embedding_dim=embedding_dim)
+        self.isbn_embedding = nn.Embedding(num_embeddings=num_isbns, embedding_dim=embedding_dim)
+        self.fc1 = nn.Linear(embedding_dim * 2, 64)
+        self.fc2 = nn.Linear(64, 1)
 
     def forward(self, users, isbns):
         user_embeds = self.user_embedding(users.long())
         isbn_embeds = self.isbn_embedding(isbns.long())
         embeds = torch.cat([user_embeds, isbn_embeds], dim=1)
         x = torch.relu(self.fc1(embeds.view(embeds.size(0), -1)))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
